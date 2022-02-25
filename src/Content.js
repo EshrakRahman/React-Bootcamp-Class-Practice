f47b34095ae7d00c45959b39910e74afc4f03854
eshrakrahman <eshrakurrahman@gmail.com>
2022-02-26 00:45:45 +0600

    ci/cd test

--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ class Content extends Component {
                 <div className="content fix">
 
                     <div className={'content-one'}>
-                        <h1>Chapter One</h1>
+                        <h1>What the hell</h1>
                         <p>Now checking ci/cd. This is a test for CI/CD. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet aperiam architecto asperiores at cum cupiditate dicta eveniet fuga inventore minus molestiae nemo nulla odio officia placeat quasi ratione, reiciendis rem repellat sapiente sed sint veniam vitae voluptas! Hello there!</p>
                     </div>
 
